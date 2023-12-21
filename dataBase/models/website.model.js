bc8fa1cdd4cbbe1c575a83a516d0654da53356fa
MinaNagy99 <minanagykhalefa@gmail.com>
2023-12-21 14:55:30 +0200

    s

--- dataBase/models/website.model.js
+++ dataBase/models/website.model.js
@@ -18,13 +18,7 @@ const schema = mongoose.Schema({
     type: String,
   },
 });
-schema.post(['find','fineOne'],(doc)=>{
-    for (let i = 0; i < doc.length; i++) {
-       doc[i].logo = 'https://cts.onrender.com/'+doc[i].logo
-       doc[i].previewImgs = doc[i].previewImgs.map((img)=>'https://cts.onrender.com/'+img)
-       doc[i].mainImg = 'https://cts.onrender.com/'+doc[i].mainImg
-    }
-})
+
 const websiteModal = mongoose.model('website',schema)
 
 export default websiteModal
