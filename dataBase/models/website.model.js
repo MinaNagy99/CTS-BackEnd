b4f5f269b10583f993d2b54e738f881e8d621845
MinaNagy99 <minanagykhalefa@gmail.com>
2023-12-28 14:01:31 +0200

    s

--- dataBase/models/website.model.js
+++ dataBase/models/website.model.js
@@ -7,13 +7,13 @@ const schema = mongoose.Schema({
   },
   previewImgs: [
     {
-      public_id: { type: String, required: true },
-      url: { type: String, required: true },
+      public_id: { type: String},
+      url: { type: String},
     },
   ],
   logo: {
-    public_id: { type: String, required: true },
-    url: { type: String, required: true },
+    public_id: { type: String},
+    url: { type: String},
   },
   title: {
     type: String,
